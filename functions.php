2389c13a6769e306b258da259b7b370db4aac60d
Douglas Reis <douglasreisofc@users.noreply.github.com>
2026-09-15 11:08:04 -0300

    Polish storefront layout and product cards

--- functions.php
+++ functions.php
@@ -17,6 +17,7 @@ function storev1_setup() {
 add_action('after_setup_theme','storev1_setup');
 function storev1_assets(){
     wp_enqueue_style('storev1-style',get_stylesheet_uri(),[],wp_get_theme()->get('Version'));
+    wp_enqueue_style('storev1-polish',get_template_directory_uri() . '/assets/storev1-polish.css',['storev1-style'],wp_get_theme()->get('Version'));
     if (is_singular() && comments_open() && get_option('thread_comments')) wp_enqueue_script('comment-reply');
 }
 add_action('wp_enqueue_scripts','storev1_assets');
